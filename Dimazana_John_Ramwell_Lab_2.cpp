e651cab3ec6a183a932c6ac06e9278512259edee
JRDIMAZANA <24-18397@g.batstate-u.edu.ph>
2025-03-15 11:17:36 +0800

    ACTIVITY 2 PART 2

--- Dimazana_John_Ramwell_Lab_2.cpp
+++ Dimazana_John_Ramwell_Lab_2.cpp
@@ -65,6 +65,77 @@ string insertAfter(string after,string data, Node *head){
     return "A new node has been added after." + after + "\n";
 }
 
+string deleteAtEnd(Node *head){
+    if(head == NULL){
+        return "The linked list is empty \n";
+    }
+
+    if(head->link == NULL){
+        delete head;
+        return "The head has been removed \n"; 
+    }
+
+    Node *temp = new Node;
+    temp = head;
+
+    while(temp->link->link != NULL){
+        temp = temp->link;
+    }
+
+    temp->link = NULL;
+
+    return "A node has been deleted at the end \n";
+}
+
+Node *deleteFromBeginning(Node *head){
+    if(head == NULL){
+        cout << "The linked list is empty \n";
+        return NULL;
+    }
+
+    if(head->link == NULL){
+        delete head;
+    }
+
+    head = head->link;
+
+    cout << "A node has been deleted from the beginning \n" << endl;
+
+    return head;
+}
+
+Node *deleteFromGivenNode(string givenNode,Node *head){
+    if(head == NULL){
+        cout << "The linked is empty. \n" << endl;
+        return NULL;
+    }
+
+    if(head->songName.compare(givenNode) == 0){
+        head = deleteFromBeginning(head);
+        cout << "The Node" + givenNode + "has been deleted. \n" << endl;
+        return head;
+    }
+
+    Node *temp = new Node;
+    Node *next = new Node;
+    temp = head;
+    next = temp->link;
+
+    while(next->songName.compare(givenNode) != 0){
+        if(temp == NULL){
+            cout << "No such node exist. \n" << endl;
+            return head;
+        }
+        next = next->link;
+        temp = temp->link;
+    }
+
+    temp->link = next->link;
+    cout << "The Node " + givenNode + " has been deleted \n" << endl;
+    return head;
+
+}
+
 
 void traverse(Node *head){
     Node *temp = new Node;
@@ -74,7 +145,7 @@ void traverse(Node *head){
     while(temp != NULL){
         cout << temp->songName << "->"<< endl;
         if(temp->link == NULL){
-            cout << "NULL" << endl;
+            cout << head->songName << endl; // Coding Challenge No.2. By replacing "NULL" by "head->songName"
         }
         
         temp = temp->link;
@@ -88,19 +159,27 @@ int main(){
 
     head = insertAtEnd("Sunday Morning by Maroon 5", head);
     head = insertAtEnd("APT. ROSÉ, Bruno Mars.", head);
-    head = insertAtEnd("Lose Control. Teddy Swims", head);
     head = insertAtEnd("Die With A Smile. Lady Gaga, Bruno Mars", head);
     head = insertAtBeginning("Stronger by TheFatRat.", head);
     head = insertAtBeginning("Language of the Lost by Riproducer.", head);
     head = insertAtBeginning("The Drum by Alan Walker.", head);
     head = insertAtBeginning("Show by Ado.", head);
-    insertAfter("Show by Ado.", "Sunday", head);
-    insertAfter("Language of the Lost by Riproducer.", "hahahka",head);
-    insertAfter("The Drum by Alan Walker", "hehehehe",head);
-    
+
+    string After = insertAfter("Show by Ado.", "Dilaw by Maki", head);
+        cout << After << endl;
+    string After1 = insertAfter("Show by Ado.", "Bughaw by Maki",head);
+        cout << After1 << endl;
+    string After2 = insertAfter("Show by Ado.", "Medyo Ako by Juan Karlos",head);
+        cout << After2 << endl;
+
+    deleteAtEnd(head);
+    head = deleteFromBeginning(head);
+    head = deleteFromGivenNode("Language of the Lost by Riproducer.",head);
 
     
+    
     traverse(head);
+  
 
    
 
